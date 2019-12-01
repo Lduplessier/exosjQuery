1eb0cd77ce9dc55084286b83cbe8e954125a0442
LDU <48243464+Lduplessier@users.noreply.github.com>
2019-12-01 20:41:56 +0100

    Add files via upload

--- exo-3/lib/slider-plugin.js
+++ exo-3/lib/slider-plugin.js
@@ -17,22 +17,43 @@
 
     // Public Methods - External methods
     Object.assign(this, {
-      'generateSlider': function() {
+      'generateImage': function() {
         this.settings.img.forEach(img => {
-          $('slider').append('<img src="' + this.settings.img[count]['src'] + '" alt="' + this.settings.img[count]['alt'] + '" id="' + this.settings.img[count]['id'] + '" class="' + this.settings.img[count]['class'] + '"');
+          $('slider').append('<img src="' + this.settings.img[count]['src'] + '" alt="' + this.settings.img[count]['alt'] + '" id="' + this.settings.img[count]['id'] + '" class="' + this.settings.img[count]['class'] + '"style= "width:100%">');
           count++;
         })
-      }
+      },
 
+      'showSlide': function(n) {
+        var i;
+        var x = document.getElementsByClassName("slides");
+        if (n > x.length) {
+          slide = 1;
+        };
+        if (n < 1) {
+          slide = x.length;
+        }
+        for (i = 0; i < x.length; i++) {
+          x[i].getElementsByClassName.display = "none";
+        }
+        x[slide-1].getElementsByClassName.display = "block";
+      },
+
+      'moveSlide': function (n) {
+        showSlide(slide += n);
+      }
     });
 
     // Private Methods - Internal methods
     Object.assign(priv, {
 
       //slider generation
-      'generator': function(form) {
-          $('body').append('<div ="slider"></div>');
-          (this).generateSlider();
+      'generator': function() {
+          $('body').append('<div class="slider"></div>');
+          $('.slider').append('<button class="button-left" onclick="moveSlide(-1)"></button>');
+          $('.slider').append('<button class="button-right" onclick="moveSlide(1)"></button>');
+          (this).generateImage();
+          (this).showSlide(slide)
         }.bind(this)
       });
 
